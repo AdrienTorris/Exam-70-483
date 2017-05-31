91b325dfa2cc80b628f57add3a5a93999bd5dabb
Adrien TORRIS <adrientorris@gmail.com>
2017-05-31 16:43:03 +0200

    System.Threading - Stopping a thread

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.04/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Exam70483CodeSamples.Threading._04
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// Stopping a thread
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stopped = false;
+
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+            }));
+
+            t.Start();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            stopped = true;
+            t.Join();
+        }
+    }
+}
