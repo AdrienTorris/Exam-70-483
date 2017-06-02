8e5a39d9edcf108d27d2a3a49de7810c38e03f3b
Adrien TORRIS <adrientorris@gmail.com>
2017-06-02 16:00:25 +0200

    System.Threading.Tasks - Adding a continuation

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exam70483CodeSamples.Threading._10
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Adding a continuation
+    /// </summary>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/system.threading.tasks.task.continuewith(v=vs.110).aspx"/>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 5;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
+
+            Console.WriteLine(t.Result);
+        }
+    }
+}
