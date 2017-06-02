2150c9beb6bc8aa5f09570565f3acf097112304d
Adrien TORRIS <adrientorris@gmail.com>
2017-06-02 15:53:33 +0200

    Starting a new Task

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.08/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exam70483CodeSamples.Threading._08
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Starting a new Task
+    /// </summary>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/system.threading.tasks(v=vs.110).aspx"/>
+    /// <remarks>
+    /// - Call Wait() is equals than calling Join() on a thread, it wait till the Task is finished before exiting the application.
+    /// </remarks>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+            {
+                for(int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("i: {0}", i);
+                }
+            });
+
+            t.Wait();
+        }
+    }
+}
