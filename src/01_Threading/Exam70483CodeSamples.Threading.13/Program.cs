843eb3d6fc6a20e2207a1a6a23e3ca63e74bca40
Adrien TORRIS <adrientorris@gmail.com>
2017-06-02 16:24:15 +0200

    Using a TaskFactory

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.13/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exam70483CodeSamples.Threading._13
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Using a TaskFactory
+    /// </summary>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/system.threading.tasks.taskfactory(v=vs.110).aspx"/>
+    /// <seealso cref="https://msdn.microsoft.com/en-us/library/dd321439(v=vs.110).aspx"/>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<Int32[]> parent = Task.Run(() =>
+            {
+                var results = new Int32[3];
+
+                TaskFactory tf = new TaskFactory(
+                    TaskCreationOptions.AttachedToParent
+                    , TaskContinuationOptions.ExecuteSynchronously);
+
+                tf.StartNew(() => results[0] = 0);
+                tf.StartNew(() => results[1] = 1);
+                tf.StartNew(() => results[2] = 2);
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(
+                parentTask =>
+                {
+                    foreach (int i in parentTask.Result)
+                        Console.WriteLine(i);
+                });
+
+            finalTask.Wait();
+        }
+    }
+}
