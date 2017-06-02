c18ea33c4055076be0e67d08bf28c94c78319ca8
Adrien TORRIS <adrientorris@gmail.com>
2017-06-02 16:52:11 +0200

    Using Task.WaitAny

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.15/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exam70483CodeSamples.Threading._15
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Using Task.WaitAny
+    /// </summary>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/dd270672(v=vs.110).aspx"/>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int>[] tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() => { Thread.Sleep(1000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(1000); return 3; });
+
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+
+                Console.WriteLine(completedTask.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
+        }
+    }
+}
