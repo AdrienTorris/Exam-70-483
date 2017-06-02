7af75746f8ddc817c6390fa676d724c980f46cc9
Adrien TORRIS <adrientorris@gmail.com>
2017-06-02 15:45:49 +0200

    Using the ThreadPool

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.07/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exam70483CodeSamples.Threading._07
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// Using the ThreadPool
+    /// </summary>
+    /// <see cref="https://msdn.microsoft.com/en-us/library/system.threading.threadpool(v=vs.110).aspx"/>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
