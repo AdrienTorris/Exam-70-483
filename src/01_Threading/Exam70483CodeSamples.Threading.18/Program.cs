efa246de3734be60ce5541c67a932bb5c0f4a830
Adrien TORRIS <adrientorris@gmail.com>
2017-06-07 14:08:32 +0200

    Using 'async' and 'await'

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.18/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exam70483CodeSamples.Threading._18
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Net.Http;
+
+    /// <summary>
+    /// Using 'async' and 'await'
+    /// </summary>
+    /// <remarks>
+    /// 
+    /// </remarks>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("https://adrientorris.github.io/index.html");
+                return result;
+            }
+        }
+    }
+}
