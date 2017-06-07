4c0977fe4ea1ce669a3e266ec802c7da8de41156
Adrien TORRIS <adrientorris@gmail.com>
2017-06-07 13:54:39 +0200

    Using 'Parallel.For' and 'Parallel.ForEach'

---
+++ src/01_Threading/Exam70483CodeSamples.Threading.16/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exam70483CodeSamples.Threading._16
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Using 'Parallel.For' and 'Parallel.ForEach'
+    /// </summary>
+    /// <remarks>
+    /// - 'Parallel' class is a static class
+    /// </remarks>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, i =>
+            {
+                Console.WriteLine("For loop. {0}", i);
+                Thread.Sleep(1000);
+              });
+
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i =>
+            {
+                Console.WriteLine("ForEach loop. {0}", i);
+                Thread.Sleep(1000);
+             });
+        }
+    }
+}
